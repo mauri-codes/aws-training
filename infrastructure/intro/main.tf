b4fa8f657c217269919b6636f5d5009046baf847
Mauricio Alarcon <mauricio.alarcon1789@gmail.com>
2023-04-09 23:48:03 -0400

    terraform intro

---
+++ infrastructure/intro/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+terraform {
+  backend "s3" {
+    # Bucket S3
+    bucket = "mauri-terraform-state"
+    key    = "introduction/terraform.ftstate"
+    region = "us-east-1"
+    # DynamoDB Table, Partition Key = LockID
+    dynamodb_table = "mau-terraform-state"
+  }
+}
+# Do terraform init again
+
+
+
+resource "aws_iam_user" "test_user" {
+  name = "my_test_user2"
+}
+
+# 2 users y un user group
+# Role
