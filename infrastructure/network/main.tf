e0904bce63b2f62b57155e11712a37838cb0c468
Mauricio Alarcon <mauricio.alarcon1789@gmail.com>
2023-04-11 12:40:07 -0400

    finished network module

--- infrastructure/network/main.tf
+++ infrastructure/network/main.tf
@@ -214,7 +214,30 @@ resource "aws_subnet" "sn_web_C" {
   }
 }
 
+resource "aws_route_table" "upb_web_rt" {
+  vpc_id = aws_vpc.main_vpc.id
 
-# Crear subnets de la lista
-# Crear Route Table
-# Asignar Route Table a subnets Web
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "upb_web_rt"
+  }
+}
+
+resource "aws_route_table_association" "association_A" {
+  subnet_id      = aws_subnet.sn_web_A.id
+  route_table_id = aws_route_table.upb_web_rt.id
+}
+
+resource "aws_route_table_association" "association_B" {
+  subnet_id      = aws_subnet.sn_web_B.id
+  route_table_id = aws_route_table.upb_web_rt.id
+}
+
+resource "aws_route_table_association" "association_C" {
+  subnet_id      = aws_subnet.sn_web_C.id
+  route_table_id = aws_route_table.upb_web_rt.id
+}
