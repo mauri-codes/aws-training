158b59df0af8b3529eff3e3a831e5e1fd5be0dd5
Mauricio Alarcon <mauricio.alarcon1789@gmail.com>
2023-04-13 13:50:04 -0400

    added outputs to network

---
+++ infrastructure/modules/network/outputs.tf
@@ -0,0 +1,6 @@
+output "vpc_id" {
+    value = aws_vpc.main_vpc.id
+}
+output "vpc_arn" {
+    value = aws_vpc.main_vpc.arn
+}
---
+++ infrastructure/network/outputs.tf
@@ -0,0 +1,6 @@
+output "vpc_id" {
+    value = module.upb_network.vpc_id
+}
+output "vpc_arn" {
+    value = module.upb_network.vpc_arn
+}
